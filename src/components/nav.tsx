5c0cc8f9caa06db355b5d331fd77a617a1fcd56e
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:17:04 -1000

    hero UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,22 @@
 const Nav = () => {
+  const handleScroll = (id: string) => {
+    const sec = document.getElementById(id);
+    sec?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
   return (
     <nav className="py-4 bg-black text-white px-4 md:px-8">
       <h1 className="font-extrabold">
         MEZAKI’S <br /> COLLECTION
       </h1>
-      <div></div>
+      <ul>
+        {links.map((item, i) => (
+          <li key={i}>
+            <button onClick={() => handleScroll(item.id)}></button>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
