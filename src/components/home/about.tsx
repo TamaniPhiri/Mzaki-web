f130895feb3de8567036ea0435a297c3545f3219
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 21:25:53 -1000

    hero UI

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <div
       id="about"
-      className=" bg-black min-h-screen text-white grid grid-cols-1 md:grid-cols-2 w-full"
+      className=" bg-black min-h-screen gap-12 text-white grid grid-cols-1 md:grid-cols-2 w-full py-12 px-4 md:px-8"
     >
       <div className="h-full flex items-center justify-center order-last md:order-first">
         <div className="h-[500px] w-fit">
@@ -15,7 +15,19 @@ const About = () => {
           />
         </div>
       </div>
-      <div className="">about</div>
+      <div className="h-full flex flex-col gap-4 justify-center md:items-start items-center md:text-left text-center">
+        <h1 className="text-4xl font-extrabold">ABOUT MEZAKI</h1>
+        <p className="text-balance max-w-sm text-gray-200">
+          Mezaki's Collection is a premier retail store based in Zambia,
+          specializing in high-quality apparel, hair products, and accessories.
+          Founded in 2020 by Zawadi Mwanza, a highly driven and professional
+          banker, Mezaki's Collection emerged from a profound passion for
+          delivering quality service and exceptional products to clients from
+          diverse backgrounds. Our mission is to provide a one-stop solution for
+          all fashion and hair care needs, ensuring every customer leaves our
+          store feeling confident and stylish.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="h-full min-h-screen grid md:grid-cols-2 grid-cols-1 px-4 md:px-8 py-12"
+      className="h-full min-h-screen gap-12 grid md:grid-cols-2 grid-cols-1 px-4 md:px-8 py-12"
     >
       <div className="h-full flex flex-col gap-4 justify-center md:items-start items-center md:text-left text-center">
         <div className="h-16 w-36">
