adf568264325638e16874b34af9bd9f6c2c969c8
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:30:38 -1000

    hero UI

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,5 +1,10 @@
 const Hero = () => {
-  return <section className="h-full">hero</section>;
+  return (
+    <section className="h-full min-h-screen grid md:grid-cols-2 grid-cols-1 px-4 md:px-8">
+      <div>1</div>
+      <div>2</div>
+    </section>
+  );
 };
 
 export default Hero;
