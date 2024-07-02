19a31bd734179c928a9ee098b6e4f45d05a18760
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 21:27:05 -1000

    hero UI

--- src/components/home/products.tsx
+++ src/components/home/products.tsx
@@ -1,5 +1,5 @@
 const Products = () => {
-  return <div className=" min-h-screen">Products</div>;
+  return <div className=" min-h-screen px-4 md:px-8">Products</div>;
 };
 
 export default Products;
