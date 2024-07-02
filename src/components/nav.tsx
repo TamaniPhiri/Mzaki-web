e2d5c88c8d5f7be66f956e475c528a4f66076509
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:21:14 -1000

    hero UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,14 +6,14 @@ const Nav = () => {
     });
   };
   return (
-    <nav className="py-4 bg-black text-white px-4 md:px-8">
+    <nav className="py-4 bg-black text-white px-4 md:px-8 flex items-center justify-between">
       <h1 className="font-extrabold">
         MEZAKI’S <br /> COLLECTION
       </h1>
-      <ul>
+      <ul className="flex items-center gap-4">
         {links.map((item, i) => (
           <li key={i}>
-            <button onClick={() => handleScroll(item.id)}></button>
+            <button onClick={() => handleScroll(item.id)}>{item.name}</button>
           </li>
         ))}
       </ul>
