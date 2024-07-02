81e038d506d42ba9f21b62c1399a91c8f1d90be8
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:38:44 -1000

    hero UI

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,7 +1,17 @@
+import logo from "../../assets/logoMzaki.png";
+
 const Hero = () => {
   return (
     <section className="h-full min-h-screen grid md:grid-cols-2 grid-cols-1 px-4 md:px-8">
-      <div>
+      <div className="h-full flex flex-col justify-center">
+        <div className="h-16 w-36">
+          <img
+            src={logo}
+            alt="logo"
+            loading="lazy"
+            className=" object-cover object-center h-full w-full"
+          />
+        </div>
         <h1>
           My Plug
           <br />
