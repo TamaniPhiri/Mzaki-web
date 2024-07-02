ddd62b731421398c39c57ff30a6267deafbb433b
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:33:59 -1000

    hero UI

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,7 +1,18 @@
 const Hero = () => {
   return (
     <section className="h-full min-h-screen grid md:grid-cols-2 grid-cols-1 px-4 md:px-8">
-      <div>1</div>
+      <div>
+        <h1>
+          My Plug
+          <br />
+          by <span>MEZAKI’S COLLECTION</span>
+        </h1>
+        <p>
+          seamlessly connect with our extensive offerings and expert service
+          providers, making it easier than ever to stay stylish and
+          well-groomed.
+        </p>
+      </div>
       <div>2</div>
     </section>
   );
