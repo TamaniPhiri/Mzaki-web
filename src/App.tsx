ef9ea593f4146d90730b16432890fcdcb09d702d
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 21:27:25 -1000

    hero UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Layout from "./components/layout";
 import Hero from "./components/home/hero";
 import About from "./components/home/about";
+import Products from "./components/home/products";
 
 function App() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Products />
     </Layout>
   );
 }
