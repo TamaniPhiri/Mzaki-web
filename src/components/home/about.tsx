72ec57694ac9dc20c333ac460cecb4acce04ea5a
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 21:18:09 -1000

    hero UI

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -1,7 +1,21 @@
+import img from "../../assets/frame5.png";
 const About = () => {
   return (
-    <div id="about" className=" bg-black min-h-screen text-white">
-      about
+    <div
+      id="about"
+      className=" bg-black min-h-screen text-white grid grid-cols-1 md:grid-cols-2 w-full"
+    >
+      <div className="h-full flex items-center justify-center">
+        <div className="h-[500px] w-fit">
+          <img
+            src={img}
+            alt="frame1"
+            loading="lazy"
+            className="object-cover object-center h-full w-full hover:rotate-3 hover:scale-95 duration-700 delay-200"
+          />
+        </div>
+      </div>
+      <div>about</div>
     </div>
   );
 };
