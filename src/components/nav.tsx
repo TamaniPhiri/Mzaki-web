7905e3af97186efb4302337d642591f7cef64873
tamani phiri <tamanigabriel0@gmail.com>
2024-07-01 20:10:41 -1000

    hero UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,9 @@
 const Nav = () => {
   return (
-    <nav className="py-4">
-      <h1>MEZAKI’S COLLECTION</h1>
+    <nav className="py-4 bg-black text-white">
+      <h1 className="font-extrabold">
+        MEZAKI’S <br /> COLLECTION
+      </h1>
     </nav>
   );
 };
