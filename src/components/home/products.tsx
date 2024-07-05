c2ab7adb9a59cc0983e7d56935bee15a4217a08c
dc-21 <cholahkuboko@gmail.com>
2024-07-05 22:46:41 +0200

    products page

--- src/components/home/products.tsx
+++ src/components/home/products.tsx
@@ -1,7 +1,10 @@
 const Products = () => {
   return (
-    <section id="products" className=" min-h-screen px-4 md:px-8 w-full">
-      Products
+    <section
+      id="products"
+      className=" min-h-screen px-4 md:px-8 w-full md:py-8 py-4"
+    >
+      <p className="text-center">Our Products</p>
     </section>
   );
 };
