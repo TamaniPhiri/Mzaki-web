f3212a1fd3b1dda8e59f0c46d0c5ee2fc11c1331
dc-21 <cholahkuboko@gmail.com>
2024-07-09 09:04:36 +0200

    footer initialization

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const footer = () => {
+  return (
+    <footer className="bg-red-900 p-4 text-white w-full flex justify-center">
+      <p>Hello</p>
+    </footer>
+  );
+};
+
+export default footer;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import Nav from "./nav";
+import Footer from "../components/footer";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex flex-col h-full min-h-screen mx-auto w-full max-w-7xl">
       <Nav />
       <main className="h-full">{children}</main>
+      <Footer />
     </section>
   );
 };
